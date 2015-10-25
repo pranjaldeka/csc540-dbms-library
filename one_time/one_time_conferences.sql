47049d8de693eb37a7293bc78089361dcc503382
Simer <sjolly@ncsu.edu>
2015-10-24 17:45:14 -0700

    Insert Scripts

---
+++ one_time/one_time_conferences.sql
@@ -0,0 +1,21 @@
+INSERT INTO SSINGH25.CONFERENCE_PAPERS VALUES
+(
+	'C1',
+	'WWW',
+	'HyeongSik Kim',
+  2013,
+	'International Journal on Semantic Web and Information'
+);
+
+INSERT INTO SSINGH25.CONFERENCE_PAPERS VALUES
+(
+	'C2',
+	'SIGMOD',
+	'Sidan Gao',
+  2013,
+	'An agglomerative query model for discovery in linked data: semantics and
+approach'
+);
+COMMIT;
+/
+
---
+++ one_time/one_time_rooms.sql
@@ -0,0 +1,94 @@
+delete from ssingh25.rooms;
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R1',
+	'LIB2',
+  2,
+  3,
+	'C'
+);
+
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R2',
+	'LIB2',
+  3,
+  3,
+	'S'
+);delete from ssingh25.rooms;
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R1',
+	'LIB2',
+  2,
+  3,
+	'C'
+);
+
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R2',
+	'LIB2',
+  3,
+  3,
+	'S'
+);
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R3',
+	'LIB1',
+  4,
+  2,
+	'S'
+);
+
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R4',
+	'LIB2',
+  3,
+  3,
+	'C'
+);
+
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R5',
+	'LIB2',
+  4,
+  3,
+	'S'
+);
+
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R6',
+	'LIB1',
+  4,
+  3,
+	'S'
+);
+
+
+INSERT INTO SSINGH25.ROOMS VALUES
+(
+	'R7',
+	'LIB2',
+  2,
+  2,
+	'S'
+);
+
+/*
+
+select * from  ssingh25.rooms;
+select * from ssingh25.libraries;
+
+
+
+
+
+
+select * from  ssingh25.rooms;
+select * from ssingh25.libraries;
+*/
