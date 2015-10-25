32e99a226f50831bde680a14ef90f9dca21f355b
Simer <sjolly@ncsu.edu>
2015-10-24 18:09:27 -0700

    Insertion Scripts Cameras

---
+++ one_time/one_time_cameras.sql
@@ -0,0 +1,37 @@
+/*describe SSINGH25.Cameras;
+*/
+delete from ssingh25.cameras;
+insert into SSINGH25.Cameras
+values 
+(
+'CA1',
+'LIB2',
+'Olympus',
+'E-620',
+'14-42mm lens 1:3.5-5.6',
+'16'
+);
+
+insert into SSINGH25.Cameras
+values 
+(
+'CA2',
+'LIB2',
+'Canon',
+'EOS Rebel T4i',
+'18-135mm EF-S IS STM Lens',
+'32'
+);
+
+insert into SSINGH25.Cameras
+values 
+(
+'CA3',
+'LIB2',
+'Canon',
+'EOS Rebel T4i',
+'18-135mm EF-S IS STM Lens',
+'32'
+);
+
+select * from SSINGH25.Cameras
