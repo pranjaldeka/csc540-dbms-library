d8d7fa71a012652fd83d3e93c184e0dd7d54e4af
devarshi112 <dsingh4@ncsu.edu>
2015-11-01 15:41:50 -0400

    changing to Utility.setMessage in Camera.java

--- CSC540_LIBRARY/src/com/ncsu/dbms/lib/resources/Camera.java
+++ CSC540_LIBRARY/src/com/ncsu/dbms/lib/resources/Camera.java
@@ -137,13 +137,13 @@ public class Camera {
 	}
 	
 	private void checkOutCameraConsole(){
-		Utility.welcomeMessage("Please enter the Camera ID of camera you want to check out");
+		Utility.setMessage("Please enter the Camera ID of camera you want to check out");
 		String cameraid = Utility.enteredConsoleString();
 		String library = null;
 		boolean flag = true;
 		do{
-			Utility.welcomeMessage("Please select the Library:");
-			Utility.welcomeMessage("1. D.H. Hill \t\t 2. J.B. Hunt");
+			Utility.setMessage("Please select the Library:");
+			Utility.setMessage("1. D.H. Hill \t\t 2. J.B. Hunt");
 			 library = Utility.enteredConsoleString();
 			if(library.equals("1") || library.equals("2"))
 				flag=false;
@@ -160,12 +160,12 @@ public class Camera {
 				String validDateFormat = "yyyy-MM-dd";
 				String validTimeFormat = "HH:mm:ss";
 				do{
-					Utility.welcomeMessage("Please enter date of return in yyyy-MM-dd format:");
+					Utility.setMessage("Please enter date of return in yyyy-MM-dd format:");
 					enteredDate = Utility.enteredConsoleString();
 					if(Utility.validateDateFormat(enteredDate, validDateFormat)){
 							flagDate = false;
 							do{
-								Utility.welcomeMessage("Please enter time of return in HH:mm:ss format:");
+								Utility.setMessage("Please enter time of return in HH:mm:ss format:");
 								enteredTime = Utility.enteredConsoleString();
 								if(Utility.validateDateFormat(enteredTime, validTimeFormat)){
 									flagTime = false;
