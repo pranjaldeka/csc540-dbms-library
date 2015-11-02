eba6f3a10b2b8f78d01538bffa792d9493241ee1
devarshi112 <dsingh4@ncsu.edu>
2015-11-02 01:38:14 -0400

    backend implementation of queue

--- packages/checkout_pkg.sql
+++ packages/checkout_pkg.sql
@@ -29,6 +29,9 @@ PROCEDURE CHECK_OUT_PROC(
 )
 IS
 avail_flag 			number(2);
+already_co_flag		number(2);
+resource_exists_in_lib_flag number(2);
+chk_user_in_q_flag	number(2);
 sql_statement		varchar2(32000);
 user_id_column 		varchar2(100);
 table_name 			varchar2(100);
@@ -98,7 +101,7 @@ BEGIN
 			sql_statement :='
 				SELECT to_timestamp('''||duedate||''',''yyyy-mm-dd HH24 mi-ss'') 
 				FROM DUAL';
-			  DBMS_OUTPUT.PUT_LINE(sql_statement);
+			  /*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
 			EXECUTE IMMEDIATE sql_statement INTO duedate_timestamp;
 			
 	
@@ -115,14 +118,14 @@ BEGIN
 					AND RESOURCE_TYPE = '''||issue_type||'''
 							';
 				
-					DBMS_OUTPUT.PUT_LINE(sql_statement);
+					/*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
 					EXECUTE IMMEDIATE sql_statement INTO valid_duration;
 		  
 					sql_statement :='
 					SELECT CURRENT_TIMESTAMP + '||valid_duration||'
 					FROM DUAL
 					';
-					DBMS_OUTPUT.PUT_LINE(sql_statement);
+					/*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
 					EXECUTE IMMEDIATE sql_statement INTO validdate_timestamp;
 					/*DBMS_OUTPUT.PUT_LINE(validdate_timestamp);
             	DBMS_OUTPUT.PUT_LINE(currentdate_timestamp);
@@ -188,6 +191,8 @@ BEGIN
 					  /*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
 					  EXECUTE IMMEDIATE sql_statement INTO primary_id;
 						  avail_flag :=0;
+						  already_co_flag := 0;
+						  resource_exists_in_lib_flag := 0;
 						  sql_statement := 
 								'SELECT 1 
 								FROM SSINGH25.' || table_name || ' T1
@@ -196,64 +201,142 @@ BEGIN
 								ON T1.'||search_parameter|| '=T2.'||search_parameter|| '
 								WHERE T2.LIBRARY_ID= '''||LIBRARY_ID||''' 
 								and T1.'||search_parameter|| '='''||ISSUE_TYPE_ID||'''
-								and not exists
-								(
+								';
+						  EXECUTE IMMEDIATE sql_statement INTO resource_exists_in_lib_flag;
+						  IF resource_exists_in_lib_flag = 1
+						  THEN
+						  BEGIN
+								sql_statement := '
 								select 1 
 								from ssingh25.'||user_table||'_CO_'||table_name||' 
 								where '||search_parameter|| '='''||ISSUE_TYPE_ID||'''
 								and '||user_id_column|| '='''||primary_id||'''
 								and return_date is null
-								)
-								and no_of_hardcopies <> 0';
-							
-							/*DBMS_OUTPUT.PUT_LINE(sql_statement);
-					*/
-					/*DBMS_OUTPUT.PUT_LINE(avail_flag);*/
-							EXECUTE IMMEDIATE sql_statement INTO avail_flag;
-							
-					IF avail_flag = 1
+								';
+								/*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
+							EXECUTE IMMEDIATE sql_statement INTO already_co_flag;
+							/*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
+							/*DBMS_OUTPUT.PUT_LINE(already_co_flag);*/
+							EXCEPTION
+								WHEN NO_DATA_FOUND THEN
+									already_co_flag := 0;
+									/*DBMS_OUTPUT.PUT_LINE(already_co_flag);*/
+								WHEN OTHERS THEN
+									output :=SQLERRM;
+							END;
+							IF already_co_flag = 1
 							THEN
-							/*
-							#######################CHECKOUT TRANSACTION#######################;
-							*/
-					  
+								OUTPUT := 'RESOURCE ALREADY ISSUED TO THE USER';
 								
-						 
-									 
+							ELSE
+							BEGIN
+								sql_statement := 
+								'SELECT 1 
+								FROM SSINGH25.' || table_name || ' T1
+								inner join 
+								SSINGH25.'||table_name||'_in_libraries T2
+								ON T1.'||search_parameter|| '=T2.'||search_parameter|| '
+								WHERE T2.LIBRARY_ID= '''||LIBRARY_ID||''' 
+								and T1.'||search_parameter|| '='''||ISSUE_TYPE_ID||'''
+									and T2.no_of_hardcopies > 0';
+									
+								EXECUTE IMMEDIATE sql_statement INTO avail_flag;
+							EXCEPTION
+								WHEN NO_DATA_FOUND THEN
+									avail_flag := 0;
+									/*DBMS_OUTPUT.PUT_LINE(avail_flag);*/
+								WHEN OTHERS THEN
+									output :=SQLERRM;
+							END;
+								/*DBMS_OUTPUT.PUT_LINE(avail_flag);*/
+								IF avail_flag = 1
+								THEN
+								/*
+								#######################CHECKOUT TRANSACTION#######################;
+								*/
+						  
+									
+							 
+										 
+										sql_statement := '
+										INSERT INTO '||user_table||'_CO_'||table_name||'
+										VALUES (
+										'''||primary_id||''',
+										'''||ISSUE_TYPE_ID||''',
+										'''||LIBRARY_ID||''',
+										TIMESTAMP'''||current_datetime||''',
+										NULL,
+					  TIMESTAMP'''||duedate||'''
+										)';
+										
+										
+										
+										EXECUTE IMMEDIATE sql_statement;
+										
+										/*update No. of avaialable hard copies*/
+										
+										sql_statement := 'UPDATE 
+										SSINGH25.'||table_name||'_in_libraries T
+										SET NO_OF_HARDCOPIES =NO_OF_HARDCOPIES-1
+										WHERE T.LIBRARY_ID= '''||LIBRARY_ID||''' 
+										and T.'||search_parameter|| '='''||ISSUE_TYPE_ID||'''';
+										/*DBMS_OUTPUT.PUT_LINE(sql_statement);
+										*/
+										EXECUTE IMMEDIATE sql_statement;
+									
+									COMMIT;
+								OUTPUT := 'CHECK OUT SUCCESSFUL';
+								
+								ELSE
+									chk_user_in_q_flag := 0;
+									OUTPUT := 'Sorry! All hard copies have been checked out.';
+									BEGIN
+									/* Checking if user already exists in queue table */
 									sql_statement := '
-									INSERT INTO '||user_table||'_CO_'||table_name||'
-									VALUES (
-									'''||primary_id||''',
-									'''||ISSUE_TYPE_ID||''',
-									'''||LIBRARY_ID||''',
-									TIMESTAMP'''||current_datetime||''',
-									NULL,
-                  TIMESTAMP'''||duedate||'''
-									)';
-									
-									
-									
-									EXECUTE IMMEDIATE sql_statement;
-									
-									/*update No. of avaialable hard copies*/
-									
-									sql_statement := 'UPDATE 
-									SSINGH25.'||table_name||'_in_libraries T
-									SET NO_OF_HARDCOPIES =NO_OF_HARDCOPIES-1
-									WHERE T.LIBRARY_ID= '''||LIBRARY_ID||''' 
-									and T.'||search_parameter|| '='''||ISSUE_TYPE_ID||'''';
-									/*DBMS_OUTPUT.PUT_LINE(sql_statement);
-									*/
-						EXECUTE IMMEDIATE sql_statement;
-								
-								COMMIT;
-							OUTPUT := 'CHECK OUT SUCCESSFUL';
-					
-					ELSE
-					OUTPUT := 'RESOURCE UNAVAILABLE/ ALREADY ISSUED TO THE USER';
-					END IF;
+										select 1 
+										from SSINGH25.RESOURCES_QUEUE
+										where 
+										RESOURCE_ID = '''||ISSUE_TYPE_ID||'''
+										and PATRON_ID = '''||primary_id||'''
+									';
+									/*DBMS_OUTPUT.PUT_LINE(sql_statement);*/
+									EXECUTE IMMEDIATE sql_statement into chk_user_in_q_flag;
+									EXCEPTION
+										WHEN NO_DATA_FOUND THEN
+											chk_user_in_q_flag := 0;
+										WHEN OTHERS THEN
+											output :=SQLERRM;
+									END;
+									/*DBMS_OUTPUT.PUT_LINE(chk_user_in_q_flag);*/
+									IF chk_user_in_q_flag = 0
+									THEN
+																			
+										/* insert into resources queue*/									
+										sql_statement := '
+											INSERT INTO SSINGH25.RESOURCES_QUEUE
+											VALUES (
+											'''||ISSUE_TYPE_ID||''',
+											'''||primary_id||''',
+											'''||USER_TYPE||''',
+											TIMESTAMP'''||current_datetime||'''
+											)';
+																			
+										EXECUTE IMMEDIATE sql_statement;		
+										COMMIT;
+
+										OUTPUT := 'Successfully added to Waiting Queue!';
+									ELSE
+										OUTPUT := 'You are already in Waiting Queue';
+									END IF;									
+								END IF;
+							
+							END IF;
+						ELSE
+							OUTPUT := 'RESOURCE IS NOT IN ANY LIBRARY';
+						END IF;
+							
 			ELSE
-					OUTPUT := 'INVALID RETURN DATE';
+					OUTPUT := 'INVALID RETURN DATE OR TIME';
 			END IF;
 	ELSE
         OUTPUT := 'INVALID PARAMETERS';
@@ -264,7 +347,7 @@ BEGIN
         OUTPUT := 'INVALID PARAMETERS';
         /*DBMS_OUTPUT.PUT_LINE('RESOURCE UNAVAILABLE/ ALREADY ISSUED TO THE USER');*/
   END IF;
-  DBMS_OUTPUT.PUT_LINE(avail_flag);
+  /*DBMS_OUTPUT.PUT_LINE(avail_flag);*/
   EXCEPTION
 	WHEN NO_DATA_FOUND THEN
     /*DBMS_OUTPUT.PUT_LINE('Invalid Inputs');*/
