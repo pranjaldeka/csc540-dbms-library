18bfe8d38c297180a5ceba0702d9f80f04d1e6f1
Simer <sjolly@ncsu.edu>
2015-11-02 20:36:21 -0800

    bug related to inactive dues fixed

--- packages/calculate_dues.sql
+++ packages/calculate_dues.sql
@@ -1,3 +1,4 @@
+/*refreshes dues*/
 set serveroutput on;
 
 CREATE OR REPLACE PACKAGE calculate_dues_pkg AS 
@@ -13,6 +14,126 @@ IS
 	IS
 	
 	BEGIN
+
+	/*
+	--Update the old dues :-
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  (trunc(coalesce(return_date,sysdate))- trunc(due_date)) * 2
+    FROM 
+
+		students_co_books S
+		
+	 WHERE patron_id = S.student_id
+					AND patron_type = 'S'
+					AND resource_type = 'B'
+					AND resource_id = S.isbn
+					AND due_start_date = due_date
+					AND due_end_date is null)
+	 
+					
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  (trunc(coalesce(return_date,sysdate))- trunc(due_date)) * 2
+    FROM 
+
+		students_co_journals S
+		
+	 WHERE patron_id = S.student_id
+					AND patron_type = 'S'
+					AND resource_type = 'J'
+					AND resource_id = S.issn
+					AND due_start_date = due_date
+					AND due_end_date is null);
+	
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  (trunc(coalesce(return_date,sysdate))- trunc(due_date)) * 2
+    FROM 
+
+		students_co_conference_papers S
+		
+	 WHERE patron_id = S.student_id
+					AND patron_type = 'S'
+					AND resource_type = 'P'
+					AND resource_id = S.conf_paper_id
+					AND due_start_date = due_date
+					AND due_end_date is null);
+					
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  extract (day from (coalesce(return_date,sysdate)-due_date)) * 24 + (extract (hour from (coalesce(return_date,sysdate)-due_date)))
+    FROM 
+
+		students_co_cameras S
+		
+	 WHERE patron_id = S.student_id
+					AND patron_type = 'S'
+					AND resource_type = 'C'
+					AND resource_id = S.camera_id
+					AND due_start_date = due_date
+					AND due_end_date is null);		
+	
+	
+	
+	-- For facultieS
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  (trunc(coalesce(return_date,sysdate))- trunc(due_date)) * 2
+    FROM 
+
+		faculties_co_books S
+		
+	 WHERE patron_id = S.faculty_id
+					AND patron_type = 'F'
+					AND resource_type = 'B'
+					AND resource_id = S.isbn
+					AND due_start_date = due_date
+					AND due_end_date is null);
+					
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  (trunc(coalesce(return_date,sysdate))- trunc(due_date)) * 2
+    FROM 
+
+		faculties_co_journals S
+		
+	 WHERE patron_id = S.faculty_id
+					AND patron_type = 'F'
+					AND resource_type = 'J'
+					AND resource_id = S.issn
+					AND due_start_date = due_date
+					AND due_end_date is null);
+	
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  (trunc(coalesce(return_date,sysdate))- trunc(due_date)) * 2
+    FROM 
+
+		faculties_co_conference_papers S
+		
+	 WHERE patron_id = S.faculty_id
+					AND patron_type = 'F'
+					AND resource_type = 'P'
+					AND resource_id = S.conf_paper_id
+					AND due_start_date = due_date
+					AND due_end_date is null);
+					
+	UPDATE patron_dues 
+    SET due_in_dollars = (SELECT  extract (day from (coalesce(return_date,sysdate)-due_date)) * 24 + (extract (hour from (coalesce(return_date,sysdate)-due_date)))
+    FROM 
+
+		faculties_co_cameras S
+		
+	 WHERE patron_id = S.faculty_id
+					AND patron_type = 'F'
+					AND resource_type = 'C'
+					AND resource_id = S.camera_id
+					AND due_start_date = due_date
+					AND due_end_date is null);		
+	*/
+	
+	
+	-- delete the active dues;
+	
+	DELETE FROM PATRON_DUES
+	WHERE due_end_date is null;
+	
+	-- Insert the new ones:-
+	
 	
 	INSERT INTO patron_dues 
 	SELECT
@@ -36,7 +157,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'B'
 					and resource_id = S.isbn
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 		UNION ALL
@@ -61,7 +182,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'J'
 					and resource_id = S.issn
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 				
@@ -88,7 +209,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'P'
 					and resource_id = S.CONF_PAPER_ID
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 				
@@ -115,7 +236,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'C'
 					and resource_id = S.camera_id
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 		
@@ -142,7 +263,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'B'
 					and resource_id = S.isbn
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 		UNION ALL
@@ -167,7 +288,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'J'
 					and resource_id = S.issn
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 				
@@ -194,7 +315,7 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'P'
 					and resource_id = S.CONF_PAPER_ID
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				)
 				
@@ -221,14 +342,14 @@ IS
 					AND patron_type = 'S'
 					AND resource_type = 'C'
 					and resource_id = S.camera_id
-					and due_start_date = due_date + interval '1' DAY
+					and due_start_date = due_date
 
 				);
-		commit;
+		
 		/* reflects the fine when a day for that fine is over */
 		DELETE FROM patron_dues
 		WHERE due_in_dollars = 0;		
- commit;
+		commit;
 END calculate_dues_proc;
 
 END calculate_dues_pkg;
